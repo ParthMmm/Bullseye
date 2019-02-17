12f3e6d3cfcf6589a146d0674a15172a54f49715
parthmangrola <parthmangrola@gmail.com>
2019-02-16 22:23:22 -0800

    Init commit

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,40 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Parth Mangrola on 2/4/19.
+//  Copyright © 2019 Parth Mangrola. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class AboutViewController: UIViewController {
+    @IBOutlet weak var webView: WKWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        if let url = Bundle.main.url(forResource: "BullsEye",
+//                                     withExtension: "html") {
+//            let request = URLRequest(url: url)
+//            webView.load(request)
+//        }
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func close(){
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,12 +9,76 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var currentValue = 0
+    var targetValue = 0
+    var score = 0
+    var round = 0
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        startNewGame()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func showAlert(){
+        
+        let difference = abs(targetValue - currentValue)
+        let points = 100 - difference
+        
+        score += points
+        
+        let title:String
+        if difference == 0{
+            title = "Perfect!"
+            score += 100
+        }
+        else if difference < 5{
+            title = "You almost had it!"
+        }
+        else if difference < 10{
+            title = "Pretty good!"
+        }
+        else{
+            title = "Not even close..."
+        }
+        
+        let message = "You scored \(points) points"
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: {_ in self.startNewRound()})
+        alert.addAction(action);
+        present(alert, animated: true, completion: nil)
+        
+    }
+    @IBAction func sliderMoved(_ slider: UISlider){
+        print("The value of the slider is now: \(slider.value)")
+        currentValue = lroundf(slider.value)
+    }
+    @IBAction func startOver(){
+        startNewGame()
+    }
+    
+    
+    func startNewRound(){
+        round+=1;
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLabels()
+    }
+    func updateLabels(){
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
+    }
+    
+    func startNewGame(){
+        score = 0
+        round = 0
+        startNewRound()
+    }
 }
 
